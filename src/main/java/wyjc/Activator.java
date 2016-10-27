e3aaec0cacc7c328693f6087628af7dc98730c15
DavePearce <david.pearce@ecs.vuw.ac.nz>
2016-10-27 14:55:31 +1300

    Added plugin support
    This adds support for the WyCC plugin system.  This means that the
    compiler can now be activated from the command-line, which is great!

--- src/main/java/wyjc/Activator.java
+++ src/main/java/wyjc/Activator.java
@@ -5,10 +5,14 @@ import java.io.*;
 import jasm.io.ClassFileReader;
 import jasm.io.ClassFileWriter;
 import jasm.lang.ClassFile;
+import wycc.lang.Command;
+import wycc.lang.Module;
+import wycc.util.Logger;
 import wyfs.lang.Content;
 import wyfs.lang.Path;
+import wyjc.commands.JvmCompile;
 
-public class Activator {
+public class Activator implements Module.Activator {
 	// =========================================================================
 	// Content Type
 	// =========================================================================
@@ -45,4 +49,69 @@ public class Activator {
 			return "class";
 		}
 	};
+
+	// =======================================================================
+	// Content Registry
+	// =======================================================================
+
+	/**
+	 * Default implementation of a content registry. This associates whiley and
+	 * wyil files with their respective content types.
+	 *
+	 * @author David J. Pearce
+	 *
+	 */
+	public static class Registry extends wyc.Activator.Registry {
+		@Override
+		public void associate(Path.Entry e) {
+			String suffix = e.suffix();
+
+			if (suffix.equals("class")) {
+				e.associate(ContentType, null);
+			} else {
+				super.associate(e);
+			}
+		}
+
+		@Override
+		public String suffix(Content.Type<?> t) {
+			return t.getSuffix();
+		}
+	}
+
+	/**
+	 * The master project content type registry. This is needed for the build
+	 * system to determine the content type of files it finds on the file
+	 * system.
+	 */
+	public final Content.Registry registry = new Registry();
+
+	// =======================================================================
+	// Start
+	// =======================================================================
+
+	@Override
+	public Module start(Module.Context context) {
+		// FIXME: logger is a hack!
+		final Logger logger = new Logger.Default(System.err);
+		// List of commands to use
+		final Command[] commands = { new JvmCompile(registry, logger) };
+		// Register all commands
+		for (Command c : commands) {
+			context.register(wycc.lang.Command.class, c);
+		}
+		// Done
+		return new Module() {
+			// what goes here?
+		};
+	}
+
+	// =======================================================================
+	// Stop
+	// =======================================================================
+
+	@Override
+	public void stop(Module module, Module.Context context) {
+		// could do more here?
+	}
 }
