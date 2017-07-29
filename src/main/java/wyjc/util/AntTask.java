8ad558e7658cd6bdf49f6cf82ffa60f54b6007d7
DavePearce <david.pearce@ecs.vuw.ac.nz>
2017-07-29 15:42:45 +1200

    Add AntTask
    This adds an ant task for running WyJC from a build.xml file.

--- src/main/java/wyjc/util/AntTask.java
+++ src/main/java/wyjc/util/AntTask.java
@@ -47,9 +47,7 @@ import wyjc.commands.JvmCompile;
  * @author David J. Pearce
  *
  */
-public class AntTask extends MatchingTask {
-
-	private JvmCompile command;
+public class AntTask extends wyc.util.AntTask {
 
 	/**
 	 * Construct a new instance of this command.
@@ -62,84 +60,13 @@ public class AntTask extends MatchingTask {
 	public AntTask() {
 		Content.Registry registry = new Activator.Registry();
 		this.command = new JvmCompile(registry, Logger.NULL);
-
 	}
 
 	// =======================================================================
 	// Configuration
 	// =======================================================================
 
-	public void setWhileypath(String dirs) throws IOException {
-		// FIXME: could use org.apache.tools.ant.types.Path path here, as ant
-		// autoconverts
-		command.setWhileypath(dirs);
-	}
-
-	public void setWhileydir(File dir) throws IOException {
-		command.setWhileydir(dir);
-	}
-
-	public void setWyildir(File dir) throws IOException {
-		command.setWyildir(dir);
-	}
-
-	public void setWyaldir(File dir) throws IOException {
-		command.setWyaldir(dir);
-	}
-
-	public void setVerify(boolean b) {
-		command.setVerify(b);
-	}
-
-	public void setVcg(boolean b) {
-		command.setVerificationConditions(b);
-	}
-
-	public void setVerbose(boolean b) {
-		command.setVerbose(b);
-	}
-
-	@Override
-	public void setIncludes(String includes) {
-		String[] split = includes.split(",");
-		Content.Filter<WhileyFile> whileyFilter = null;
-		for (String s : split) {
-			if (s.endsWith(".whiley")) {
-				String name = s.substring(0, s.length() - 7);
-				Content.Filter<WhileyFile> nf1 = Content.filter(name, WhileyFile.ContentType);
-				whileyFilter = whileyFilter == null ? nf1 : Content.or(nf1, whileyFilter);
-			}
-		}
-		if (whileyFilter != null) {
-			command.setIncludes(whileyFilter);
-		}
-	}
-
-	@Override
-	public void setExcludes(String excludes) {
-
-	}
-
-	// =======================================================================
-	// Execute
-	// =======================================================================
-
-	@Override
-	public void execute() throws BuildException {
-		try {
-			List<Path.Entry<WhileyFile>> files = command.getModifiedSourceFiles();
-			Compile.Result r = command.execute(files);
-			if (r == Compile.Result.SUCCESS) {
-				if(command.getVerify()) {
-					log("Compiled and Verified " + files.size() + " source file(s)");
-				} else {
-					log("Compiled " + files.size() + " source file(s)");
-				}
-			} else {
-				throw new BuildException();
-			}
-		} catch (Exception e) {
-			throw new BuildException(e);
-		}
+	public void setClassdir(File dir) throws IOException {
+		((JvmCompile)command).setClassdir(dir);
 	}
 }
